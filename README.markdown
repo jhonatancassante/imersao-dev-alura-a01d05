03f5c0fb83d83b558d49ffe17f9c2be781609198
Jhonatan Casssante <64996354+jhonatancassante@users.noreply.github.com>
2022-02-08 16:26:53 -0300

    Add files via upload

---
+++ README.markdown
@@ -0,0 +1,7 @@
+# Aula 01 / Desafio 05 - Calculadora de média
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/jhonatancassante/pen/LYLLwXg](https://codepen.io/jhonatancassante/pen/LYLLwXg).
+
+Calculadora de Média simples. Calcula a média de dois valores digitados pelo usuário e exibe na tela do navegador.
+
+Desafio 5. Reduzir o número de variáveis
---
+++ script.js
@@ -0,0 +1,22 @@
+var notaPrimeiroBimestre = 9;
+var notaSegundoBimestre = 7;
+var notaTerceiroBimestre = 4;
+var notaQuartoBimestre = 2;
+
+console.log(
+  "Nota final: " +
+    (
+      (notaPrimeiroBimestre +
+        notaSegundoBimestre +
+        notaTerceiroBimestre +
+        notaQuartoBimestre) /
+      4
+    ).toFixed(2)
+);
+
+//Isso é um comentário
+
+//Revisão
+//Variáveis, string, console.log, toFixed, operações matemáticas, concatenação
+
+//Desafio 5. Reduzir o número de variáveis
